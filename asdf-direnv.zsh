2afa22ac1f29de9a0cae170c95e628e7921e67c5
Jan Sorensen <sorensenjan@gmail.com>
2021-04-11 08:54:07 +0200

    reformat

--- asdf-direnv.zsh
+++ asdf-direnv.zsh
@@ -1,6 +1,8 @@
+# Use direnv instead of shims. Direnv asdf plugin must be added
 hook="$(asdf exec direnv hook zsh)"
 if [[ $? == 0 ]]; then
-# Hook direnv into your shell.
+    # Hook direnv into your shell.
+
     eval $hook
 
     # A shortcut for asdf managed direnv.
