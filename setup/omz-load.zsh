59ff5ff815da95e3e8976c2e5260cd9a36c34bb7
Jan Sorensen <sorensenjan@gmail.com>
2021-04-21 22:00:45 +0200

    Init p10k in omz-load script

--- setup/omz-load.zsh
+++ setup/omz-load.zsh
@@ -26,3 +26,9 @@ plugins=(
   )
 
 source $ZSH/oh-my-zsh.sh
+
+autoload -U compinit && compinit
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
