b5b5c13727ac4640a42f820663293da5be46f428
Jan Sorensen <sorensenjan@gmail.com>
2021-04-22 08:11:36 +0200

    Check for dir present in setup

--- setup/omz-load.zsh
+++ setup/omz-load.zsh
@@ -1,8 +1,17 @@
 # Source from .zshrc to init oh-my-zsh
+# like: source $ZSH/custom/setup/omz-load.zsh
 
 export PATH=$HOME/bin/share:/bin:$PATH
 export MY_TOOLS_DIR=~/.tools
 export ZSH_THEME="powerlevel10k/powerlevel10k"
+
+baseNameIfExist() {
+  local customPluginDir="$ZSH/custom/plugins"
+  local dir="$customPluginDir/$1"
+  if [[ -d "$dir" ]]; then
+    print $1
+  fi
+}
 
 plugins=(
   my-init
@@ -18,12 +27,12 @@ plugins=(
   ripgrep
   thefuck
   # Seperate installed plugins
-  fzf-tab
-  zsh-completions
-  zsh-syntax-highlighting
+  $(baseNameIfExist fzf-tab)
+  $(baseNameIfExist zsh-completions)
+  $(baseNameIfExist zsh-syntax-highlighting)
   # gcloud must come after zsh-completions
   gcloud
-  )
+)
 
 source $ZSH/oh-my-zsh.sh
 
@@ -31,4 +40,3 @@ autoload -U compinit && compinit
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-
