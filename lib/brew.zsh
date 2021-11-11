6e23d217117901599cde38bbc46ec8e8115845cc
Jan Sorensen <sorensenjan@gmail.com>
2021-11-11 03:52:14 +0100

    Add linuxbrew init to brew lib

--- lib/brew.zsh
+++ lib/brew.zsh
@@ -1,3 +1,7 @@
+if [ -f /home/linuxbrew/.linuxbrew/bin/brew ] ; then
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
+
 hash brew &>/dev/null || return
 
 [[ -z "$HOMEBREW_PREFIX" ]] && echo "HOMEBREW_PREFIX not defined"
