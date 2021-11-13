9690b324098a283f9435ee56d9238f778fdeff8e
Jan Sorensen <sorensenjan@gmail.com>
2021-11-13 14:40:52 +0100

    Define global git ignore

--- setup/sys-init.sh
+++ setup/sys-init.sh
@@ -26,4 +26,7 @@ cp $p10kSrc $p10kTarget
 
 #echo "Linking dot files..."
 #linkdot "p10k.zsh"
+
+echo "Define global gitignore"
+git config --global core.excludesfile $(cd $(dirname $0) ; pwd)/gitignore_global
  
