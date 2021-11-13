32613fe6395b2dd2016ffb0a43a0315c511576a4
Jan Sorensen <sorensenjan@gmail.com>
2021-11-13 20:22:31 +0100

    exa: ignore .git and .DS_Store

--- lib/ls-replacements.zsh
+++ lib/ls-replacements.zsh
@@ -3,7 +3,7 @@ if (( ${+commands[lsd]} )) ; then
 fi
 
 if (( ${+commands[exa]} )) ; then
-    alias+ exa --git -a --icons --group-directories-first
+    alias+ exa --git -a --icons --group-directories-first --ignore-glob=".git\|.DS_Store"
     # Change color of date column (da), git modified (gm) in long listings
     export EXA_COLORS="da=37:gm=31:di=36"
     alias ls=exa
