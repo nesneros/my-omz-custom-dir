7330d6fddb5aede4fa620272bbebb6b2818088f7
Jan Sorensen <sorensenjan@gmail.com>
2021-11-13 08:44:29 +0100

    Move p10k.zsh to dotfiles dir

--- setup/sys-init.sh
+++ setup/sys-init.sh
@@ -6,5 +6,24 @@ cd $(dirname $0)/..
 
 echo "OMZ Custom install dir: $(pwd)"
 
-cd brew
-brew bundle
+( cd brew ; brew bundle )
+
+dotFilesDir=$(pwd)/dotfiles
+
+linkdot() {
+    file=$1
+    from=$dotFilesDir/$file
+    to=$HOME/.$file
+    echo " - linking $file ($to -> $from)"
+    ln -sf $from $to    
+}
+
+p10kSrc=$dotFilesDir/p10k.zsh
+p10kTarget=$HOME/.p10k.zsh
+
+echo "Overwriting $p10kTarget with $p10kSrc"
+cp $p10kSrc $p10kTarget
+
+#echo "Linking dot files..."
+#linkdot "p10k.zsh"
+ 
