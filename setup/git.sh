15c47f9a222012b7381312a0f0917d48bbe29c00
Jan Sorensen <sorensenjan@gmail.com>
2021-11-19 09:28:55 +0100

    setup git to use delta

--- setup/git.sh
+++ setup/git.sh
@@ -1,5 +1,16 @@
-# Sublime merge as git mergetool
+#! /usr/bin/env bash
 
-git config --global mergetool.smerge.cmd 'smerge mergetool "$BASE" "$LOCAL" "$REMOTE" -o "$MERGED"'
-git config --global mergetool.smerge.trustExitCode true
-git config --global merge.tool smerge
+set +e
+
+if hash delta >/dev/null; then
+    git config --global pager.diff delta
+    git config --global pager.show delta
+    git config --global pager.log delta
+    git config --global pager.blame delta
+    git config --global pager.reflog delta
+    git config --global interactive.diffFilter delta --color-only
+fi
+
+#git config --global mergetool.smerge.cmd 'smerge mergetool "$BASE" "$LOCAL" "$REMOTE" -o "$MERGED"'
+#git config --global mergetool.smerge.trustExitCode true
+#git config --global merge.tool smerge
