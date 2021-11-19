de0dea2aecab39a893aac48e7bc3acedf8b030e9
Jan Sorensen <sorensenjan@gmail.com>
2021-11-19 10:36:16 +0100

    More git settings for delta

--- setup/git.sh
+++ setup/git.sh
@@ -9,6 +9,11 @@ if hash delta >/dev/null; then
     git config --global pager.blame delta
     git config --global pager.reflog delta
     git config --global interactive.diffFilter delta --color-only
+
+    git config --global delta.features decorations
+    git config --global delta.line-numbers true
+    git config --global delta.side-by-side false
+    git config --global delta.interactive.keep-plus-minus-markers false
 fi
 
 #git config --global mergetool.smerge.cmd 'smerge mergetool "$BASE" "$LOCAL" "$REMOTE" -o "$MERGED"'
