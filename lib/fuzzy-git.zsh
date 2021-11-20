bbf1ee2273072c35d9d9f6a661a6b9687e6fb584
Jan Sorensen <sorensenjan@gmail.com>
2021-11-20 16:37:26 +0100

    fuz alias

--- lib/fuzzy-git.zsh
+++ lib/fuzzy-git.zsh
@@ -1,3 +1,4 @@
 if [[ -f $MY_TOOLS_DIR/git-fuzzy/bin/git-fuzzy ]]; then
-    export PATH="$MY_TOOLS_DIR/git-fuzzy/bin:$PATH" 
+    export PATH="$MY_TOOLS_DIR/git-fuzzy/bin:$PATH"
+    alias fuz="git fuzzy"
 fi
