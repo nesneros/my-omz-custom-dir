10cbc527d9ae18545887cecccd19cd5ff0451540
Jan Sorensen <sorensenjan@gmail.com>
2021-12-02 18:27:04 +0100

    more git settings

--- setup/git.sh
+++ setup/git.sh
@@ -2,7 +2,17 @@
 
 set +e
 
+gc() {
+    git config --global "$1" "$2"
+}
+
+alias() {
+    gc "alias.$1" "$2"
+}
 if hash delta >/dev/null; then
+    gc color.ui auto
+    gc core.pager cat
+        
     git config --global pager.diff delta
     git config --global pager.show delta
     git config --global pager.log delta
@@ -14,6 +24,10 @@ if hash delta >/dev/null; then
     git config --global delta.line-numbers true
     git config --global delta.side-by-side false
     git config --global delta.interactive.keep-plus-minus-markers false
+
+    alias aliases "!git config --get-regexp alias | sed -re 's/alias\\.(\\S*)\\s(.*)$/\\1 = \\2/g'"
+    alias s status
+    alias lg "log --graph --date=relative --pretty=tformat:'%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%an %ad)%Creset'"
 fi
 
 #git config --global mergetool.smerge.cmd 'smerge mergetool "$BASE" "$LOCAL" "$REMOTE" -o "$MERGED"'
