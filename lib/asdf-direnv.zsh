a2e95c2f3bf658c922f605c6b4337ce34f7adbea
Jan Sorensen <sorensenjan@gmail.com>
2021-12-02 20:54:59 +0100

    use asdfrc file part of project

--- lib/asdf-direnv.zsh
+++ lib/asdf-direnv.zsh
@@ -3,21 +3,26 @@ if (( ! ${+commands[asdf]} )); then
   return
 fi
 
+export ASDF_CONFIG_FILE=$ZSH_CUSTOM/dotfiles/asdfrc
 ASDF_DIR=$(brew --prefix asdf)
 source $ASDF_DIR/libexec/asdf.sh
 
+export DIRENV_BIN=$(asdf where direnv)/bin/direnv
+
+[[ -x "$DIRENV_BIN" ]] || return
+
 # Use direnv instead of shims. Direnv asdf plugin must be added
-hook="$(asdf exec direnv hook zsh)"
+hook="$($DIRENV_BIN hook zsh)"
 if [[ $? == 0 ]]; then
-    # Hook direnv into your shell.
+  # Hook direnv into your shell.
 
-    eval $hook
+  eval $hook
 
-    # A shortcut for asdf managed direnv.
-    direnv() { asdf exec direnv "$@"; }
+  # A shortcut for asdf managed direnv.
+  direnv() { $DIRENV_BIN "$@"; }
 
-    #echo export PATH=$PATH:$ASDF_DIR/bin
+  #echo export PATH=$PATH:$ASDF_DIR/bin
 
-    # In addition ~/.config/direnv/direnvrc is updated
+  # In addition ~/.config/direnv/direnvrc is updated
 fi
 unset hook
