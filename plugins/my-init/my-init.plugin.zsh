87344de14ed902a43d4d2a4dede3949d66d365f5
Jan Sorensen <sorensenjan@gmail.com>
2021-12-08 21:41:33 +0100

    cleanup

--- plugins/my-init/my-init.plugin.zsh
+++ plugins/my-init/my-init.plugin.zsh
@@ -24,24 +24,6 @@ if (( ${+commands[brew]} )) ; then
   FPATH=${HOMEBREW_PREFIX}/share/zsh/site-functions:$FPATH
 fi
 
-# Spefify where gcloud is installed. This is used by ohmyzsh gcloud plugin
-# The logic below is heavy inspired by the glcoud plugin
-
-# Set CLOUDSDK_HOME
-if [[ -z "${CLOUDSDK_HOME}" ]]; then
-  search_locations=(
-    $HOMEBREW_PREFIX/Caskroom/google-cloud-sdk/latest/google-cloud-sdk
-    #    $HOME/.asdf/installs/gcloud/$(asdf current gcloud | awk '{ print $2 }')
-  )
-
-  for gcloud_sdk_location in $search_locations; do
-    if [[ -d "${gcloud_sdk_location}" ]]; then
-      CLOUDSDK_HOME="${gcloud_sdk_location}"
-      break
-    fi
-  done
-fi
-
 # Set fzf base, used for fzf plugin
 if [[ -z "$FZF_BASE" ]] && (( ${+commands[fzf]} )); then
   case $OSTYPE in
