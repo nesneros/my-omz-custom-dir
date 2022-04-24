8597127d137ca3f88e2056ee9a3b788a9a2d38d6
Jan Hvid Sørensen <sorensenjan@gmail.com>
2022-04-24 14:53:46 +0200

    Fix in asdf init

--- lib/asdf.zsh
+++ lib/asdf.zsh
@@ -2,5 +2,7 @@ if (( ! ${+commands[asdf]} )); then
   return
 fi
 
+# this should be default for ASDF_DEFAULT_TOOL_VERSIONS_FILENAME but explicit setting it removes from errors. Eg for asdf direnv envrc
+export ASDF_DEFAULT_TOOL_VERSIONS_FILENAME=.tool-versions
 export ASDF_CONFIG_FILE=$ZSH_CUSTOM/dotfiles/asdfrc
 export ASDF_GOLANG_DEFAULT_PACKAGES_FILE=$ZSH_CUSTOM/dotfiles/default-golang-pkgs
