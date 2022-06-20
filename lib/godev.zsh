d2b4c01e580386177549564a3922b593a2c5acf1
Jan Hvid Sørensen <sorensenjan@gmail.com>
2022-06-20 08:30:57 +0200

    Don't check for panicparse in godev

--- lib/godev.zsh
+++ lib/godev.zsh
@@ -1,15 +1,18 @@
 # https://github.com/maruel/panicparse
 # Default panicparse is installed as 'pp'. For installation use: go install github.com/maruel/panicparse/v2@latest
 
-if (( ${+commands[panicparse]} )) ; then
-    alias -g PP="|& panicparse"
+# panicparse is install is asdf managed go so it wont work to guard setup with a check if panicparse is defined
+#if (( ${+commands[panicparse]} )) ; then
+    alias -g PP=" |& panicparse"
     
     function gorunpp {
         go run "$@" |& panicparse
     }
-fi
 
-export GOTRACEBACK=all
+    alias gm="gorunpp main.go"
+#fi
+
+export GOTRACEBACK=1
 
 # See https://pkg.go.dev/runtime for values for GODEBUG
 export GODEBUG=""
