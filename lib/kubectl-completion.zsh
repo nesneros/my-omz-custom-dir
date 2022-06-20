666b446e0373d0f1a00105fddb063bc86d8d6acb
Jan Hvid Sørensen <sorensenjan@gmail.com>
2022-06-20 08:31:32 +0200

    Remove warning from omz setup

--- lib/kubectl-completion.zsh
+++ lib/kubectl-completion.zsh
@@ -1,5 +1,5 @@
 if (( $+commands[kubectl] )); then
-    __KUBECTL_COMPLETION_FILE="${ZSH_CACHE_DIR}/kubectl_completion_$(kubectl version --client | base64)"
+    __KUBECTL_COMPLETION_FILE="${ZSH_CACHE_DIR}/kubectl_completion_$(kubectl version --client 2> /dev/null | sha1sum | base64)"
 
     if [[ ! -f $__KUBECTL_COMPLETION_FILE || ! -s $__KUBECTL_COMPLETION_FILE ]]; then
         kubectl completion zsh >! $__KUBECTL_COMPLETION_FILE
