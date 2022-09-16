2b6be97dc1b43c6f2528f088cca410e00a56697f
Jan Hvid Sørensen <sorensenjan@gmail.com>
2022-09-16 21:32:22 +0200

    comment in godev

--- lib/godev.zsh
+++ lib/godev.zsh
@@ -12,6 +12,15 @@
     alias gm="gorunpp main.go"
 #fi
 
+# https://golang.org/pkg/runtime/#hdr-Environment_Variables
+# GOTRACEBACK=1
+# The default. Prints a stack trace of all current goroutines.
+# GOTRACEBACK=2
+# Prints a stack trace of all current goroutines and the goroutines that created them.
+# GOTRACEBACK=crash
+# Prints the stack trace and then crashes with a non-zero exit code.
+# GOTRACEBACK=system
+# Prints the stack trace and then crashes with a non-zero exit code. The difference between this and GOTRACEBACK=crash is that the latter will not call the crash handler, which may be useful for debugging the crash handler itself.
 export GOTRACEBACK=1
 
 # See https://pkg.go.dev/runtime for values for GODEBUG
