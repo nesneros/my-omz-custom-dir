fb886a1ab71e279d235555cc6e41416d286c4e77
Jan Hvid Sørensen <sorensenjan@gmail.com>
2022-12-19 14:46:06 +0100

    qrypt dir by convention

--- load-lib.zsh
+++ load-lib.zsh
@@ -1,3 +1,11 @@
+# Auto detect some local configs
+
+for dir ($HOME/.crypt $HOME/Documents/.qrypt); do
+  if [ -d "$dir" ] ; then
+     export QRYPT_DIR="$dir"
+  fi
+done
+
 # Load all of your custom configurations from custom/lib
 for config_file ($ZSH_CUSTOM/lib/*.zsh(N)); do
   source $config_file
