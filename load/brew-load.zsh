a6db606967b0fa39e1eedd879426615e95e50ba8
Jan Hvid Sørensen <sorensenjan@gmail.com>
2023-02-10 09:58:26 +0100

    Fix brew-load

---
+++ load/brew-load.zsh
@@ -0,0 +1,17 @@
+if [[ -z "$HOMEBREW_PREFIX" ]] ; then
+  brewLocations=(/opt/homebrew /usr/local /home/linuxbrew/.linuxbrew)
+  for e in $brewLocations; do
+    brewBin=$e/bin/brew
+    if [[ -x "$brewBin" ]] ; then
+      eval $($brewBin shellenv)
+      break
+    fi
+    unset brewBin
+  done
+  unset brewLocations
+fi
+
+if (( ${+commands[brew]} )) ; then
+  [[ -z "$HOMEBREW_PREFIX" ]] && echo "HOMEBREW_PREFIX not defined"
+  FPATH=${HOMEBREW_PREFIX}/share/zsh/site-functions:$FPATH
+fi
