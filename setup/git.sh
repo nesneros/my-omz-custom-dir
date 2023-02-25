2f2bfc8e433fa60b503a0940e5a708a68ef3c8a0
Jan Sorensen <sorensenjan@gmail.com>
2023-02-25 19:54:58 +0100

    Add githooks

--- setup/git.sh
+++ setup/git.sh
@@ -9,33 +9,35 @@ gc() {
 alias() {
     gc "alias.$1" "$2"
 }
+
 if hash delta >/dev/null; then
-    gc color.ui auto
-    gc core.pager cat
-        
-    git config --global pager.diff delta
-    git config --global pager.show delta
-    git config --global pager.log delta
-    git config --global pager.blame delta
-    git config --global pager.reflog delta
-    git config --global interactive.diffFilter delta --color-only
+    gc pager.diff delta
+    gc pager.show delta
+    gc pager.log delta
+    gc pager.blame delta
+    gc pager.reflog delta
+    gc interactive.diffFilter delta --color-only
 
-    git config --global delta.features decorations
-    git config --global delta.line-numbers true
-    git config --global delta.side-by-side false
-    git config --global delta.interactive.keep-plus-minus-markers false
-
-    alias aliases "!git config --get-regexp alias | sed -re 's/^alias\.([a-z-]+) /\1:\t/g' | column -t -s $'\t'"
-    alias s "status --short"
-    alias lg "log --graph --date=relative --pretty=tformat:'%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%an %ad)%Creset'"
-    alias wt "worktree"
-
-    alias hide "update-index --assume-unchanged"
-    alias hide-all "!git diff --name-only | xargs git hide"
-    alias unhide "update-index --no-assume-unchanged"
-    alias hidden "!git ls-files -v | grep '^[[:lower:]]' | cut -c 3-"
-    alias unhide-all "!git hidden | xargs git update-index --no-assume-unchanged"
+    gc delta.features decorations
+    gc delta.line-numbers true
+    gc delta.side-by-side false
+    gc delta.interactive.keep-plus-minus-markers false
 fi
+
+gc color.ui auto
+gc core.pager cat
+gc core.hooksPath $ZSH_CUSTOM/githooks/
+
+alias aliases "!git config --get-regexp alias | sed -re 's/^alias\.([a-z-]+) /\1:\t/g' | column -t -s $'\t'"
+alias s "status --short"
+alias lg "log --graph --date=relative --pretty=tformat:'%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%an %ad)%Creset'"
+alias wt "worktree"
+
+alias hide "update-index --assume-unchanged"
+alias hide-all "!git diff --name-only | xargs git hide"
+alias unhide "update-index --no-assume-unchanged"
+alias hidden "!git ls-files -v | grep '^[[:lower:]]' | cut -c 3-"
+alias unhide-all "!git hidden | xargs git update-index --no-assume-unchanged"
 
 #git config --global mergetool.smerge.cmd 'smerge mergetool "$BASE" "$LOCAL" "$REMOTE" -o "$MERGED"'
 #git config --global mergetool.smerge.trustExitCode true
