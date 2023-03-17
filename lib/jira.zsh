ac6da98a87030ca6c20e811b9d1fa27660112674
Jan Sorensen <sorensenjan@gmail.com>
2023-03-17 13:49:27 +0100

    jira cleanup

--- lib/jira.zsh
+++ lib/jira.zsh
@@ -17,15 +17,24 @@ alias j=jira
 alias jmyopen='jira issue list -a$(command jira me) --order-by rank --reverse -s~done'
 alias jmyopen-select='jira issue list -a$(command jira me) --order-by rank -s~done | fzf --tac | awk "{ print \$2 }" '
 
-jopen() {
+_jira_issue_id() {
     id=$1
     if [[ -z "$id" ]]; then
-        currentBranchName=$(git branch --show-current)
-        id=$(git config --get "branch.$currentBranchName.commitScope") || :
+        currentBranchName=$(git branch --show-current) || return 1
+        id=$(git config --get "branch.$currentBranchName.commitScope")
     fi
     if [[ -z "$id" ]]; then
-        echo "No Jira ticket found"
+        echo "No Jira issue id found" > /dev/stderr
         return 1
     fi
-    jira open $id
+    echo $id
+}
+
+jopen() {
+    jira open $(_jira_issue_id $1)
+}
+
+jira-summary() {
+    id=$(_jira_issue_id $1) || return 1
+    jira issue view $id | grep '^  #' | sed 's/^  # *//'
 }
